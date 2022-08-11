90ea3f67fe1e4388bf91222d36e122c790359a30
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-08-11 17:25:44 -0300

    fix: likesControllers

--- src/routes/likesRouter.js
+++ src/routes/likesRouter.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import idSchema from "../schemas/idSchema.js";
+import paramsSchema from "../schemas/paramsSchema.js";
 import { validateSchema } from "./../middlewares/schemaValidator.js";
 import tokenValidator from "../middlewares/tokenValidator.js";
 import { createLike, dislike } from "../controllers/likesController.js";
 
 const likesRouter = Router();
-
-likesRouter.delete("/likes", validateSchema(idSchema), tokenValidator, dislike);
 
 likesRouter.post(
   "/likes",
@@ -14,5 +13,7 @@ likesRouter.post(
   tokenValidator,
   createLike
 );
+
+likesRouter.delete("/likes/:id", tokenValidator, dislike);
 
 export default likesRouter;
