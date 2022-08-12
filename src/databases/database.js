0f376b33bf01fdd4125895d1c1b64eebbe9727a9
RobertaDolabella <dolabella.roberta@gmail.com>
2022-08-11 21:39:37 -0300

    preparado para merge.

--- src/databases/database.js
+++ src/databases/database.js
@@ -1,37 +1,20 @@
-// import pg from "pg";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const { Pool } = pg;
-// const configDatabase = {
-//   connectionString: process.env.DATABASE_URL
-// };
-
-// if(process.env.MODE === "PROD") {
-//   configDatabase.ssl = {
-//     rejectUnauthorized: false
-//   }
-// }
-
-// const db = new Pool(configDatabase);
-// export default db;
-
-
-import pkg from 'pg';
-import dotenv from 'dotenv';
-
-const { Pool } = pkg;
-
+import pg from "pg";
+import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Pool({
+const { Pool } = pg;
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL
+};
 
-host : "localhost",
-port : 5432 ,
-user :"postgres",
-password : "admin",
-database : "liniker"
-});
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false
+  }
+}
 
-
+const db = new Pool(configDatabase);
 export default db;
+
+
+
